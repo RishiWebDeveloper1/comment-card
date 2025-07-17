3d733e9aef8d8d9c3acb7ca28ed25ecd20e45494
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2025-07-17 15:05:29 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,16 +2,27 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const User = require('./model/user.js')
+require('dotenv').config();
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
-mongoose.connect('');
+app.use(cors({
+    origin: [ "http://localhost:5173" ],
+    credentials: true
+}));
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get('/get', (req, res) => {
     User.find()
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
+
+
+app.listen(3000, () => {
+    console.log("Server is started on port 3000");
+});
--- server/model/user.js
+++ server/model/user.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema(
     {
         username: { type: String, required: true },
+        userImage: { type: String, required: true },
         role: { type: String, required: true },
         comment: { type: String, required: true },
         task: { type: String, required: true },
-        review: { type: String, required: true },
-        star: { type: String, required: true },
-        star: { type: String, required: true }
+        stars: { type: String, required: true },
+        reviews: { type: String, required: true }
     }
 );
 
